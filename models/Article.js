b32a0dc3cdb68473721cc069a586fc6b313802cc
Luke DeLeon <lukedeleon94@gmail.com>
2019-10-28 23:25:49 -0700

    added handlebars

--- models/Article.js
+++ models/Article.js
@@ -1,24 +1,30 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
+
 var ArticleSchema = new Schema({
-    // `title` is required and of type String
+    category: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
     },
-    // `link` is required and of type String
     link: {
         type: String,
         required: true
     },
-    // `note` is an object that stores a Note id
-    // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with an associated Note
+    summary: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
     note: {
         type: Schema.Types.ObjectId,
         ref: "Note"
